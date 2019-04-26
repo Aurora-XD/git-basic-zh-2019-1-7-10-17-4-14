554bfc4628f2cca194f5562f24026d49b61df77f
yangchen <1053364992@qq.com>
2019-04-27 00:34:55 +0800

    配置文件中修改gradle为bin文件，section1本地测试全部通过

--- src/main/java/cn/school/thoughtworks/section1/PracticeB.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeB.java
@@ -1,10 +1,16 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PracticeB {
     List<String> collectSameElements(List<String> collection1, List<List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        //选出1集合中与2集合中子数组的元素相同的元素
+        List<String> resultList = new ArrayList<String>();
+        for (List<String> strings : collection2) {
+            collection1.forEach(obj -> {if(strings.contains(obj)){ resultList.add(obj);}});
+        }
+        return resultList;
     }
 }
--- src/main/java/cn/school/thoughtworks/section1/PracticeC.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeC.java
@@ -1,11 +1,17 @@
 package cn.school.thoughtworks.section1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class PracticeC {
     List<String> collectSameElements(List<String> collection1, Map<String,List<String>> collection2) {
         //实现练习要求，并改写该行代码。
-        return null;
+        List<String> resultList = new ArrayList<>();
+        for(Object key : collection2.keySet()){
+            List<String> temp = collection2.get(key);
+            collection1.forEach(obj -> {if(temp.contains(obj)){resultList.add(obj);}});
+        }
+        return resultList;
     }
 }
